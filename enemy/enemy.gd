43a3ad1686f28f8d8084e1f3df0eb4b6fbc934b3
Ferencz Hollander <f.m.t.hollander@student.tudelft.nl>
2024-11-09 15:12:46 +0100

    Added enemy dummy start

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
+@export var patrol_path: Path2D
+@export var move_speed = 100
+var patrol_offset = 0
+var patrol_length = 0
+var is_done_moving = false
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func _ready():
+	if !patrol_path:
+		queue_free()
+	patrol_length = patrol_path.curve.get_baked_length()
+	move_speed /= 10
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if !is_done_moving:
+		move_enemy(delta)
+	
+func move_enemy(delta: float):
+	patrol_offset += move_speed
+	position = patrol_path.curve.sample_baked(patrol_offset)
+	if patrol_offset >= patrol_length:
+		is_done_moving = true
