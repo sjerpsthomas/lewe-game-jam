bee795059912be31da001bc32d141ffaa530cccc
Ferencz Hollander <f.m.t.hollander@student.tudelft.nl>
2024-11-10 01:09:00 +0100

    added projectile framework

---
+++ projectiles/jassie_projectiles/projectile_jassie_low.gd
@@ -0,0 +1,10 @@
+class_name JassieProjectileLow
+extends JassieProjectile
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	damage = 5
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ projectiles/projectile.gd
@@ -0,0 +1,19 @@
+class_name Projectile
+extends Node2D
+
+var damage = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+		queue_free()
